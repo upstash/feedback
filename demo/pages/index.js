dbd8a2bdcf0960c95242124465577a556115beeb
Adem ilter <ademilter@gmail.com>
2022-01-24 21:04:44 +0300

    fix demo

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -3,36 +3,35 @@ import FeedbackWidget from '@upstash/feedback'
 export default function IndexPage() {
   return (
     <div className="p-10">
-      adem
       <pre className="mb-4">
         {`
-      <FeedbackWidget>
-        <button>Feedback</button>
-      </FeedbackForm>`}
+<FeedbackWidget>
+  <button>Feedback</button>
+</FeedbackWidget>`}
       </pre>
       <FeedbackWidget>
         <button className="py-1 px-2 bg-blue-600 text-white rounded">
           Feedback
         </button>
       </FeedbackWidget>
-      <hr className="my-20" />
-      {/*      <pre className="mb-4">*/}
-      {/*        {`*/}
-      {/*<FeedbackForm*/}
-      {/*  user="steve.jobs@apple.com"*/}
-      {/*  metadata={{ location: 'Palo Alto' }}*/}
-      {/*>*/}
-      {/*  <button>Feedback</button>*/}
-      {/*</FeedbackForm>`}*/}
-      {/*      </pre>*/}
-      {/*      <FeedbackForm*/}
-      {/*        user="steve.jobs@apple.com"*/}
-      {/*        metadata={{ location: 'Palo Alto' }}*/}
-      {/*      >*/}
-      {/*        <button className="py-1 px-2 bg-blue-600 text-white rounded">*/}
-      {/*          Feedback with user*/}
-      {/*        </button>*/}
-      {/*      </FeedbackForm>*/}
+      <hr className="my-10" />
+      <pre className="mb-4">
+        {`
+<FeedbackWidget
+  user="steve.jobs@apple.com"
+  metadata={{ location: 'Palo Alto' }}
+>
+  <button>Feedback with user</button>
+</FeedbackWidget>`}
+      </pre>
+      <FeedbackWidget
+        user="steve.jobs@apple.com"
+        metadata={{ location: 'Palo Alto' }}
+      >
+        <button className="py-1 px-2 bg-blue-600 text-white rounded">
+          Feedback with user
+        </button>
+      </FeedbackWidget>
     </div>
   )
 }
